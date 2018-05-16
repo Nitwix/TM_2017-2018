be8a647b299bdaa1051975e7e95bd5350aa7ba26
Niels Lachat <nielsnfsmw@gmail.com>
2018-05-16 20:24:32 +0200

    suppression de app_src

--- app_src/scripts/states/boot.js
+++
@@ -1,9 +0,0 @@
-var bootState = {};
-
-bootState.create = function(){
-	//mode de plein écran (utilisé dans mainMenu.js et plus tard)
-	game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
-
-
-	game.state.start("preload");
-}
